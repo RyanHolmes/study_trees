df596827185164462a1c15894b67605b2fd363bb
Ryan Holmes <rholmes@Ryans-MacBook-Pro.local>
2016-02-13 18:01:04 -0500

    ..

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var jquery = require('jquery');
+
+var app = express();
+// app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.post('/data', function(req, res){
+  res.sendStatus(200);
+
+  fs.writeFile(__dirname + '/public/javascripts/data.js', "function getData() {\n" + "var data =" + req.body.value + ";\n" + "return data;}", function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+});
+
+app.listen(3000, function(){
+  console.log("running on port localhost:3000");
+});
